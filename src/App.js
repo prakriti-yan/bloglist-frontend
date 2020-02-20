f5e398323a4b1ec4336ed6f601acc0b5ae8ab246
Yan Yan <yyan@c2r2p13.hive.fi>
2020-02-20 15:02:27 +0200

    new feature added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,63 @@ import React, { useState, useEffect } from 'react';
 import loginService from './services/login'
 import blogService from './services/blogs'
 
+const BlogForm = (props) =>{
+  const { createBlog, title, setTitle, author, setAuthor, url, setUrl} = props
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={createBlog}>
+      <div>
+        title:
+        <input
+        type="text"
+        value={title}
+        name="Title"
+        onChange={({target})=> setTitle(target.value)}/>
+      </div>
+      <div>
+        author:
+        <input 
+        type = "text"
+        value = {author}
+        name = "Author"
+        onChange={({target})=> setAuthor(target.value)}/>
+      </div>
+      <div>
+        url:
+        <input 
+        type = "text"
+        value = {url}
+        name = "Url"
+        onChange={({target})=> setUrl(target.value)}/>
+      </div>
+      <button type= "submit">create</button>
+    </form>
+    </div>
+  )
+}
+
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   
   useEffect(()=>{
     getBlogs()
+  }, [])
+
+  useEffect(()=>{
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const getBlogs = async() =>{
@@ -19,8 +67,24 @@ function App() {
     console.log(blogs)
   }
 
+  const createBlog = async event =>{
+    event.preventDefault()
+    try{
+       const newBlog = await blogService.create({
+        'title': title,
+        'author': author,
+        'url': url
+      })
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    }catch(exception){
+      console.log(exception)
+    }
+  }
+
   const handleLogin = async (event) =>{
-    console.log("logging in!")
     event.preventDefault()
     try{
       const user = await loginService.login({
@@ -31,16 +95,22 @@ function App() {
       window.localStorage.setItem(
         'loggedBlogUser', JSON.stringify(user)
       )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
-      console.log(user.username)
+
     }catch(exception){
       setErrorMessage('Wrong credientials')
       setTimeout(()=>{
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () =>{
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser(null)
   }
 
   const printBlog = () =>blogs.map(blog=>{
@@ -80,10 +150,17 @@ function App() {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} is logged in</p>
-        <>
-        {printBlog()}
-        </>
+      <p>{user.username} is logged in! <button onClick={handleLogout}>logout</button></p>
+      <br></br>
+        <BlogForm 
+        createBlog={createBlog}
+        title={title}
+        setTitle={setTitle}
+        author={author}
+        setAuthor={setAuthor}
+        url={url} setUrl={setUrl}/>
+        {/* {printBlog()} */}
+        
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,9 +2,29 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api/blogs'
 // const baseUrl = 'http://localhost:3003/api/users'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const setToken = newToken =>{
+  token = `bearer ${newToken}`
 }
 
-export default { getAll }
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const create = async newObject =>{
+  const config = {
+    headers: {Authorization: token}
+  }
+  // console.log('config is :', config)
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = async (id, newObject)=>{
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
+}
+
+export default { getAll, create, setToken, update }
