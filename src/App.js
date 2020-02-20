0c048b1bc3690f821fdbe66b094383530ed9e9bc
Yan Yan <yyan@c2r2p7.hive.fi>
2020-02-20 12:00:44 +0200

    blog list added

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import loginService from './services/login'
 import blogService from './services/blogs'
-
-// const Form = (props) => {
-//   // const 
-//   return (
-//     <form onSubmit={handleLogin}>
-//       <div>
-//         username
-//         <input
-//         type="text"
-//         value={username}
-//         name="Username"
-//         onChange={({target})=> setUsername(target.value)}/>
-//       </div>
-//       <div>
-//         password
-//         <input 
-//         type = "text"
-//         value = {password}
-//         name = "Password"
-//         onChange={({target})=> setPassword(target.value)}/>
-//       </div>
-//       <button type= "submit">login</button>
-//     </form>
-//   )
-// }
 
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [blog, setBlog] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  
+  useEffect(()=>{
+    getBlogs()
+  }, [])
+
+  const getBlogs = async() =>{
+    const blogs = await blogService.getAll()
+    setBlogs(blogs)
+    console.log(blogs)
+  }
 
   const handleLogin = async (event) =>{
     console.log("logging in!")
     event.preventDefault()
-    // console.log("name and password are", username, password)
     try{
-      const user = loginService.login({
+      const user = await loginService.login({
         username: username, 
         password: password
       })
       setUser(user)
       setUsername('')
       setPassword('')
-      console.log(user)
-      // blogDisplay()
+      console.log(user.username)
     }catch(exception){
       setErrorMessage('Wrong credientials')
       setTimeout(()=>{
@@ -56,11 +39,14 @@ function App() {
     }
   }
 
-  // const blogDisplay =() =>{
-  //   const newBlog = blogService.getAll()
-  //   setBlog(newBlog)
-  //   console.log(blog)
-  // }
+  const printBlog = () =>blogs.map(blog=>{
+    return (
+      <p key={blog.id}>
+        {blog.title}  {blog.author}
+      </p>
+    )
+  })
+  
 
   if (user === null){
   return ( 
@@ -90,7 +76,10 @@ function App() {
   return (
     <div>
       <h2>blogs</h2>
-      <p>Here are the blogs</p>
+      <p>{user.username} is logged in</p>
+        <>
+        {printBlog()}
+        </>
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api/blogs'
+// const baseUrl = 'http://localhost:3003/api/users'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
