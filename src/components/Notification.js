d017f69776e4c84efbfcea2cfc21c157015b400d
Yan Yan <yyan@c2r2p9.hive.fi>
2020-02-28 14:32:59 +0200

    delete comments

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,25 +1,17 @@
 import React from 'react'
 
-const Notification = (props) =>{
-	const {notice, success} = props
-	const noticeStyle = {
-		  color: success? 'green' : 'Red',
-		  background:'lightgrey',
-		  fontSize: 20,
-		  borderStyle: 'solid',
-		  borderRadius: 5,
-		  padding: 10,
-		  marginBottom: 10
-	}
-	if (notice !== null){
-	return(
-	  <div style= {noticeStyle}>
-		  {notice}
-	  </div>
-	)}
-	else{
-	  return null
-	}
+const Notification = (props) => {
+  const { notice, success } = props
+  const style = success ? 'notice'  :  'alert'
+  if (notice !== null){
+    return(
+      <div className={style}>
+        {notice}
+      </div>
+    )}
+  else{
+    return null
   }
+}
 
-  export default Notification
+export default Notification
--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3003/api/login'
+const baseUrl = '/api/login'
 
 const login = async credential => {
   const response = await axios.post(baseUrl, credential)
